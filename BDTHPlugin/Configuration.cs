ac2c52f0387571978c52f704094822affcfc387f
James Stine <leon.blade@gmail.com>
2021-04-21 23:16:21 -0400

    Add sort by distance bool to config

--- BDTHPlugin/Configuration.cs
+++ BDTHPlugin/Configuration.cs
@@ -11,6 +11,7 @@ namespace BDTHPlugin
         public bool UseGizmo { get; set; } = false;
         public bool DoSnap { get; set; } = false;
         public float Drag { get; set; } = 0.05f;
+        public bool SortByDistance { get; set; } = false;
 
         [NonSerialized]
         private DalamudPluginInterface pluginInterface;
